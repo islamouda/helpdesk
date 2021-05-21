5045c94267a254de5c6cec5c7785a36f83c144d6
islamouda <33865170+islamouda@users.noreply.github.com>
2021-05-21 16:48:05 +0300

    change the URL for tickit

--- resources/views/close_email_template.blade.php
+++ resources/views/close_email_template.blade.php
@@ -22,7 +22,7 @@
         <h4 class="display-4 ld" >Hi {{ $user }}</h4>
         <p class="lead"> Ticket was closed by {{ $user_id }}</p>
         <hr >
-        <a href="http://172.16.0.177/ticket/{{ $ticket->id }}"><p>ticket</p></a>
+        <a href="https://helpdesk.metcoapp.com/ticket/{{ $ticket->id }}"><p>ticket</p></a>
         <p class="text-center">Title :{{ $title }}</p>
         <p>Subject :{{ $subject }}</p>
         <p>replay :{{ $ticket->replay }}</p>
--- resources/views/dynamic_email_template.blade.php
+++ resources/views/dynamic_email_template.blade.php
@@ -21,7 +21,7 @@
         <h1 class="display-4 ld" >Help Desk</h1>
         <p class="lead">This is a Ticket From .{{ $user }}</p>
         <hr>
-        <a href="http://172.16.0.177/ticket/{{ $ticket }}"><p>Click here to show Ticket</p></a>
+        <a href="https://helpdesk.metcoapp.com/ticket/{{ $ticket }}"><p>Click here to show Ticket</p></a>
         <p class="text-center">Title :{{ $title }}</p>
         <p>Subject :{{ $subject }}</p>
        
